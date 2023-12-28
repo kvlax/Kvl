ae391e9f4a35c812d0b9b3e6f292b5786b9d1464
kvlax <154732042+kvlax@users.noreply.github.com>
2023-12-28 18:15:44 +0100

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,27 @@
+<?php
+$db_host = "your_database_host";
+$db_user = "your_database_username";
+$db_password = "your_database_password";
+$db_name = "mydatabase";
+
+$conn = new mysqli($db_host, $db_user, $db_password, $db_name);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    $sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Registration successful!";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
+?>
