683636a19831a91120f74aa7e3c7df9868bb46b7
kvlax <154732042+kvlax@users.noreply.github.com>
2023-12-28 18:16:27 +0100

    Update mydatabase.sql

--- mydatabase.sql
+++ mydatabase.sql
@@ -4,6 +4,5 @@ USE mydatabase;
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
 );
